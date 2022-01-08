332adeb891cbf9b42aacee0d8377e0442f8a7718
orsond <orsondmc@gmail.com>
2022-01-08 14:09:18 +1100

    Javadoc

--- src/main/java/com/collarmc/pounce/EventDispatcher.java
+++ src/main/java/com/collarmc/pounce/EventDispatcher.java
@@ -1,5 +1,12 @@
 package com.collarmc.pounce;
 
+/**
+ * Implementors can dispatch events
+ */
 public interface EventDispatcher {
+    /**
+     * Dispatch an event
+     * @param event to dispatch
+     */
     void dispatch(Object event);
 }
